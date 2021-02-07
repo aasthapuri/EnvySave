22b45751b3ea5c0711fa0f5a96fad462ec219f16
aasthapuri <aasthapuri28@gmail.com>
2021-02-08 03:53:10 +0530

    EnvySave

--- app/src/main/java/com/example/envy_save/HomeActivity.java
+++ app/src/main/java/com/example/envy_save/HomeActivity.java
@@ -1,14 +1,63 @@
  package com.example.envy_save;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
-public class HomeActivity extends AppCompatActivity {
+ public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        WebView webView = (WebView) findViewById(R.id.WebView);
+        webView.loadUrl("http://envy-save.c1.biz/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient(){
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result){
+                AlertDialog dialog = new AlertDialog.Builder(view.getContext()).setTitle("Saves").setMessage(message).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                })
+                        .create();
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.setCancelable(false);
+                dialog.show();
+                return true;
+
+
+
+            }
+            public boolean onJsConfirm(WebView view,String url,String message,JsResult result){
+                AlertDialog dialog = new AlertDialog.Builder(view.getContext()).setTitle("Save").setMessage(message).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        result.confirm();
+                    }
+                })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                result.cancel();
+                            }
+                        })
+                        .create();
+                dialog.setCanceledOnTouchOutside(false);
+                dialog.setCancelable(false);
+                dialog.show();
+                return true;
+            }
+        }
+
+        );
+
     }
 }
